a8fd05149a465074fa8ff5f3e50f9c6ed89bd3f3
Fatimazohra95 <fatimazohralakhal32@gamil.com>
2023-11-23 03:08:01 -0800

    Airb

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Simple flask server with only one end point"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_holberton():
+    """
+    route to / to Display Hello HBNB on any request
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
